07a90dfd263fc4ed9218f984d89510ba4cf6f8e8
ahul5777 <79792474+ahul5777@users.noreply.github.com>
2022-07-12 14:21:24 +0530

    Add files via upload

--- mynewpath.js
+++ mynewpath.js
@@ -1,7 +1,8 @@
 import { api, LightningElement, track, wire } from 'lwc';
-import { getRecord, getFieldValue, updateRecord } from 'lightning/uiRecordApi';
+import { getRecord, getFieldValue, updateRecord,getRecordNotifyChange } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { getPicklistValuesByRecordType, getObjectInfo } from 'lightning/uiObjectInfoApi';
+import getpicklistvalues from '@salesforce/apex/pathClassHandler.getpicklistvalues';
 
 const fields = [];
 var DUMMY_RECORDTYPE_ID = '012000000000000AAA';
@@ -13,14 +14,15 @@ export default class PathComponent extends LightningElement {
     @api fieldApiName; /* field api name. For example, Rating, Status__c */
     @api values; /* picklist values comma separated if the values are not part of picklist field */
     @api currentValue; /* current value of the field */
+    @track newValue;
     pathType='path'; /* type of the path. For base, path */
     @api buttonLocation; /* location of the button. For top, same row */
     @api buttonLabel = 'Update Current Value'; /* label of the button */
-    @api showButton = false;
+    @api showButton = false;  
     @api recordTypeName; /* record type name */
     @track openModal;
     @track closedStatus='';
-
+    @track isUpdaterunning=false;
     @track pathValues = [];
     isLoading = false;
     isTop = false;
@@ -39,8 +41,14 @@ export default class PathComponent extends LightningElement {
         ];
     }
 
+    async handler()
+    {
+        getRecordNotifyChange([{recordId:'$this.recordId'}]).response(response=> {this.getValues();console.log('Inside Notify:');}).catch(error=>{console.log('Error:', error.body.message)});
+    }
+
     connectedCallback() {
 
+        
         fields.push(this.objectApiName+'.'+this.fieldApiName);
         if(this.recordTypeName){
             fields.push(this.objectApiName+'.RecordTypeId');
@@ -56,38 +64,26 @@ export default class PathComponent extends LightningElement {
         }else{
             //this.handlePicklistValues();
         }
-    }
 
-    /* get the current value for the piklist field */
-    @wire(getRecord, { recordId: '$recordId', fields })
-    getfieldValue({error, data}){
 
-        if(data){
-            let fieldValue    = getFieldValue(data, this.objectApiName+'.'+this.fieldApiName);
-            this.recordTypeId = getFieldValue(data, this.objectApiName+'.RecordTypeId');
-            if(!this.currentValue){
-                let closedStatus=['UnQualified','Disqualified','Not Responsive','Not Interested'];
-               if(closedStatus.includes(fieldValue))
-               {
-                  this.currentValue='Closed'; 
-               }
-               else
-               {
-                this.currentValue = fieldValue;
-               }
-                
-            }
-            if(this.recordTypeId){
-                //DUMMY_RECORDTYPE_ID = this.recordTypeId;
-            }else{
-                this.recordTypeId = DUMMY_RECORDTYPE_ID;
-            }
-
-        }else if(error){
-            this.isError = true;
-            this.errorMessage = error.body.message;
+        if(!this.isUpdaterunning)
+        {
+            this.getValues();
         }
+
+
+        
+
+
     }
+
+    renderedCallback()
+    {
+        updateRecord({fields: {Id:this.recordId}});
+      
+    }
+
+ 
 
     @wire(getPicklistValuesByRecordType, { objectApiName: '$objectApiName', recordTypeId: '$recordTypeId' })
     fetchValues({error, data}){
@@ -121,16 +117,48 @@ export default class PathComponent extends LightningElement {
 
     }
 
+    getValues()
+    {
+    
+    console.log('Inside piclist');
+    console.log('Prinitng Record Id',this.recordId);
+    getpicklistvalues({recordId:this.recordId}).
+        then(response=>
+            {
+                console.log('Response Recived');
+                console.log(response);
+
+                let fieldValue=response;
+                let closedStatus=['UnQualified','Disqualified','Not Responsive','Not Interested'];
+                if(closedStatus.includes(fieldValue))
+                {
+                   this.currentValue='Closed'; 
+                }
+                else
+                {
+                 this.currentValue = fieldValue;
+                }
+    
+            }).
+        catch(error=>
+            {
+                console.log(error.body.message);
+                console.log('ERROR');
+            });
+        }
+
     optionChanged(event)
     {
         let value = event.detail.value;
         this.newValue = value;
-        console.log(this.currentValue);
+        console.log('Printing new value after comboox value changed',this.newValue);
+        
 
     }
     handleSelectChange(event){
      
         //event.preventDefault();
+        this.isUpdaterunning=true;
         this.currentValue = event.target.value;
         console.log(this.currentValue);
         if(this.currentValue==='Closed')
@@ -151,17 +179,19 @@ export default class PathComponent extends LightningElement {
     }
 
     handleClick(event){
-        event.preventDefault();
+        this.isUpdaterunning=true;
+        //event.preventDefault();
         this.isLoading = true;
         const fields = {};
         fields['Id'] = this.recordId; //ID_FIELD.fieldApiName
 
      
         console.log('printing new value',this.newValue);
-        console.log('printing current ');
+        console.log('printing current ',this.currentValue);
         if(this.newValue)
         {
-            fields[this.fieldApiName] = this.newValue; 
+            fields[this.fieldApiName] = this.newValue;
+            this.newValue=null; 
        }
        else
        {
@@ -178,6 +208,7 @@ export default class PathComponent extends LightningElement {
                     variant: 'success'
                 })
             );
+            this.isUpdaterunning=false;
         })
         .catch(error => {
             console.error(' Error updating record ', error);
@@ -187,10 +218,13 @@ export default class PathComponent extends LightningElement {
                     message: error.body.message,
                     variant: 'error'
                 })
+                
             );
+            this.isUpdaterunning=false;
         })
         .finally(() => {
             this.isLoading = false;
+            
         });
     }
 }
